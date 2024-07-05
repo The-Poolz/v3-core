4ae4db5f093490839b256155f97a3de03252240a
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2024-07-05 16:39:03 +0300

    update IBNB interface

--- contracts/interfaces/IBNBParty.sol
+++ contracts/interfaces/IBNBParty.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.7.6;
-pragma abicoder v2;
 
 interface IBNBParty {
     /// 'msg.sender' is pool contract
     /// can get tokens addresses by using token0, token1 functions
     /// can get token balances by using default 'balanceOf' function
-    function handleSwap() external;
+    /// @param recipient address who will receive the tokens
+    function handleSwap(address recipient) external;
 }
